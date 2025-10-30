f27cfcc43b4477e716c36cb0dc292807d1c00bf8
Adrian Razini Rangel <160901383+AdrainRazini@users.noreply.github.com>
2025-10-29 21:04:35 -0300

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -4,14 +4,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/11.5.0/firebase
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey:"AIzaSyAFcTol_ZewYrft-wflOdgNEPn6kzJ5qpo",
-  authDomain:"data-server-ids.firebaseapp.com",
-  projectId:"data-server-ids",
-  storageBucket:"data-server-ids.firebasestorage.app",
-  messagingSenderId:"878117229677",
-  appId:"1:878117229677:web:7b008a3fa3c6fd59811b64",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
-
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
@@ -19,3 +18,4 @@ const db = getFirestore(app);
 const provider = new GoogleAuthProvider();
 
 export { app, auth, db, provider };
+
